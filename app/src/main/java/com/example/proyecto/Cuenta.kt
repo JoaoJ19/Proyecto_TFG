f1fc28f8e47c6c71eae4c8e0603f1cdca0116b20
joao josimar farias ortega <Josymar>
2024-05-25 11:10:10 +0200

    Todo correcto Inicio

--- app/src/main/java/com/example/proyecto/Cuenta.kt
+++ app/src/main/java/com/example/proyecto/Cuenta.kt
@@ -1,29 +1,57 @@
 package com.example.proyecto
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.example.proyecto.R
-import com.example.proyecto.databinding.FragmentCuentaBinding
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
+class CuentaPruebita : AppCompatActivity() {
+    private lateinit var emailTextView: TextView
+    private lateinit var passwordTextView: TextView
+    private lateinit var nombreTexto: TextView
+    private lateinit var botonSalir: Button
+    private val firebaseAuth = FirebaseAuth.getInstance()
 
-class Cuenta : Fragment() {
-    private var _binding: FragmentCuentaBinding? = null
-    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cuenta_pruebita)
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cuenta, container, false)
+        emailTextView = findViewById(R.id.elcorreo)
+        passwordTextView = findViewById(R.id.lacontrasena)
+        nombreTexto = findViewById(R.id.fotoPerfildesc)
+        botonSalir = findViewById(R.id.salir)
+
+        val email = intent.getStringExtra("email")
+        val password = intent.getStringExtra("password")
+        val nombre = intent.getStringExtra("email")
+        nombreTexto.text = nombre?.substringBefore("@") ?: "Correo no disponible"
+        emailTextView.text = email ?: "Correo no disponible"
+        passwordTextView.text = password ?: "Contraseña no disponible"
+
+        botonSalir.setOnClickListener {
+            showLogoutConfirmationDialog()
+        }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun showLogoutConfirmationDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("¿Seguro que quieres cerrar sesión?")
+            .setPositiveButton("Sí") { dialog, id ->
+                // Cerrar sesión y volver a MainActivity
+                firebaseAuth.signOut()
+                val intent = Intent(this@CuentaPruebita, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+                finish()
+            }
+            .setNegativeButton("No") { dialog, id ->
+                // Cancelar el diálogo
+                dialog.dismiss()
+            }
+        val alert = builder.create()
+        alert.show()
     }
 }
--- app/src/main/java/com/example/proyecto/Inicio.kt
+++ app/src/main/java/com/example/proyecto/Inicio.kt
@@ -1,28 +1,31 @@
 package com.example.proyecto
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.example.proyecto.databinding.FragmentInicioBinding
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.navigation.NavController
 
-class Inicio : Fragment() {
+class otroInicio : AppCompatActivity() {
 
-    private var _binding: FragmentInicioBinding? = null
-    private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_dentro, container, false)
-    }
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otro_inicio)
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+
+        val naturalRecipesCardView: CardView = findViewById(R.id.monumentosCosta)
+        val snackRecipesCardView: CardView = findViewById(R.id.restaurantesCosta)
+
+        naturalRecipesCardView.setOnClickListener {
+            val intent = Intent(this, InicioPruebita::class.java)
+            startActivity(intent)
+        }
+
+        snackRecipesCardView.setOnClickListener {
+            val intent = Intent(this, InicioPruebita::class.java)
+            startActivity(intent)
+        }
     }
 }
